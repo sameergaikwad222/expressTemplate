7a3ff7185bc63f373421cc50f90c0e4cd1dff434
Sameer Gaikwad <sameergaikwad222@gmail.com>
2024-01-29 07:03:38 +0530

    Added Swagger

--- app.js
+++ app.js
@@ -3,19 +3,35 @@ const { connectMongoDB } = require("./src/database/database");
 const express = require("express");
 const { resourceRouter, healthRouter } = require("./src/routes");
 const app = express();
+const process = require("node:process");
+const helmet = require("helmet");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./src/swagger.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 async function initApp() {
-  await connectMongoDB();
   const PORT = config?.port || 3000;
   try {
+    await connectMongoDB();
+
+    // Middlewares
     app.use(express.json());
+    app.use(helmet());
+    app.use(cors());
+
+    // Routers
     app.use("/resource", resourceRouter);
     app.use("/health", healthRouter);
+
+    // Start The Server
     app.listen(PORT, () => {
       console.log(`App Started on http://localhost:${PORT}`);
     });
   } catch (error) {
     console.log(`Error while starting an App, ${error.message}`);
+    process.exit(1);
   }
 }
 
--- src/database/database.js
+++ src/database/database.js
@@ -5,10 +5,12 @@ async function connectMongoDB() {
   try {
     await mongoose.connect(dbConfig.database.url, {
       dbName: dbConfig.database.dbName,
+      connectTimeoutMS: 30,
     });
     console.log("Database connected Successfully");
   } catch (error) {
     console.log(`Error while connecting database ${error.message}`);
+    throw error;
   }
 }
 
